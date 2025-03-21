af0cf293225b2891b05b6e0705bbc8017e08afdb
hariV0078 <145679215+hariV0078@users.noreply.github.com>
2025-03-21 20:23:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-import google.generativeai as genai  # Corrected import
+from google import genai  # Corrected import
 from groq import Groq
 from pydantic import BaseModel
 from typing import Optional
